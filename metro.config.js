3c0c369683cf597503f0d4b9d7c31e0f1820d228
Iyk Azorji <iykazorji@gmail.com>
2025-03-20 18:31:02 -0400

    update metro config to default to file based imports when handling @nobles/hashes/crypto

--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
-
+const path = require("path");
 const config = getDefaultConfig(projectRoot);
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [projectRoot];
 
 // Important to allow importing package exports
 config.resolver.unstable_enablePackageExports = true;
@@ -16,5 +13,22 @@ config.resolver.unstable_conditionNames = [
 	"require",
 	"react-native",
 ];
+
+const ALIASES = {
+	"@noble/hashes/crypto": path.resolve(
+		__dirname,
+		"node_modules/@noble/hashes/crypto.js"
+	),
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+	if (ALIASES[moduleName]) {
+		return {
+			filePath: ALIASES[moduleName],
+			type: "sourceFile",
+		};
+	}
+	return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
