faa7143049349d12c2a4e03f59eda46d43aaf1fc
Arman Mamyan <armmamyan@gmail.com>
2025-07-30 10:28:42 +0400

    Fix: Upgrade package versions, Setup proper env variable handling

--- src/utils/signer.ts
+++ src/utils/signer.ts
@@ -1,7 +1,8 @@
 import { RNAlchemySigner } from "@account-kit/react-native-signer";
+import Constants from "expo-constants";
 
 export const signer = RNAlchemySigner({
-	client: { connection: { apiKey: process.env.EXPO_PUBLIC_API_KEY ?? "" } },
+	client: { connection: { apiKey: Constants.expoConfig?.extra?.EXPO_PUBLIC_ALCHEMY_API_KEY ?? "" } },
 	// optional param to override session settings
 	sessionConfig: {
 		// sets the expiration to 60 minutes
